c6f5d449b64377cac9d5500f17ef810db13a6751
kezike <kezike13@gmail.com>
2019-01-24 08:32:05 -0500

    Corrected some errors in README.md; Committed some files that were somehow forgotten

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This package contains a [Verifiable Credentials](https://w3c.github.io/vc-data-m
 To get started with `solid-vc`, navigate to your local `solid-vc` directory and run `npm run setup`. This script takes care of creating keypairs, authenticating you with Solid, setting up necessary folders in your POD, and anything else you could possibly need to use the platform.
 
 ### Launch SolidVC App
-In order to launch the SolidVC application, open "http://localhost:PORT", where PORT is typically a 4-5 digit number that is reported to you as an output of `npm start` from above stage above. On startup, you will be prompted to login again.
+In order to launch the SolidVC application, run `npm start` and open "http://localhost:PORT", where PORT is typically a 4-5 digit number that is reported to you as an output of `npm start`. (Note: On startup, you will be prompted to login again)
--- auth/publish_key.sh
+++ auth/publish_key.sh
@@ -6,20 +6,24 @@
 # $3 - local public key query file
 # $4 - profile document card (without fragments like '#me')
 
+# Refer to rest directory to access POST and PATCH
+restDir=$PWD/../rest
+
 # Read appropriate configuration parameters
-configFile=$1
+configFile=$PWD/../config.json
+jsonApi=$PWD/../api/json.js
 pubKeyFolderRemoteKey=PUB_FOLDER_REMOTE
+pubKeyFileRemoteKey=PUB_FILE_REMOTE
 pubKeyFileLocalKey=PUB_FILE_LOCAL
 pubKeyQueryFileKey=PUB_QUERY_FILE
 profileKey=SOLID_PROFILE
-
 pubKeyFolderRemote=`node $jsonApi --read --key=$pubKeyFolderRemoteKey --json=$configFile`
 pubKeyFileLocal=`node $jsonApi --read --key=$pubKeyFileLocalKey --json=$configFile`
 pubKeyQueryFile=`node $jsonApi --read --key=$pubKeyQueryFileKey --json=$configFile`
 profile=`node $jsonApi --read --key=$profileKey --json=$configFile`
 
-cd ../rest
-./post.sh $pubKeyFolderRemote $pubKeyFileLocal
+# Post public key to Solid pod
+$restDir/post.sh $pubKeyFolderRemote $pubKeyFileLocal
 
 # Extract relative file name
 IFS='/'
@@ -34,9 +38,13 @@ fileNameNumPartsTwo=${#fileNamePartsTwo[@]}
 fileName=${fileNamePartsTwo[0]}
 fileExt=${fileNamePartsTwo[1]}
 IFS=$OIFS
-pubKeyUri=$pubKeyFolderRemote/$fileName
+pubKeyUri=$pubKeyFolderRemote/$fileName.txt
+
+# Write pub key file to config file for future reference
+node $jsonApi --write --key=$pubKeyFileRemoteKey --value=$pubKeyUri --json=$configFile
 
 # Update profile document to point to public key
 echo "INSERT DATA { <> <https://w3id.org/security#publicKey> \"$pubKeyUri\" }" > $pubKeyQueryFile
-./patch.sh $profile $pubKeyQueryFile
-cd ../auth
+$restDir/patch.sh $profile $pubKeyQueryFile
+rm $pubKeyQueryFile
+
